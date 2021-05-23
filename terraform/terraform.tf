1f0bae8f2bfa10d33484c933eca9834f87885346
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-05-23 21:32:19 +0200

    Update provider to 0.14.2

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 0.15.0"
 
   required_providers {
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
-      version = ">= 0.14.1"
+      version = ">= 0.14.2"
     }
     random = {
       source = "random"
